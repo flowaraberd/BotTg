44cdefd7565b9d8cfdacc56a6e8f6f7e13927503
flowaraberd <flowaraberd@gmail.com>
2022-09-04 07:17:18 -0300

    Agregando nueva version con linea de comando v1.0

--- Config.py
+++ Config.py
@@ -2,6 +2,6 @@
 class M:
     MY_TOKEN_BOT = '5688833678:AAFFVG7lLa5wvJmlXyLMoeCw9BFjD6xoL-U'
     MY_USER_ID_BOT = '5688833678'
-    MY_USER_ID = ''
+    MY_USER_ID = '1822798056'
     MSG_FINISH = 'El Bot, ya no esta escuchando...'
     
