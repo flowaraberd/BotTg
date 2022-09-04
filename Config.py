8a212140a78ffd280f0336fcc7a3ece35fc0929a
flowaraberd <67209635+flowaraberd@users.noreply.github.com>
2022-09-04 07:28:08 -0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,9 +1,8 @@
 
 class M:
+    #Este es un nuevo Bot!, por si el otro ya no te funciona
     MY_TOKEN_BOT = '5688833678:AAFFVG7lLa5wvJmlXyLMoeCw9BFjD6xoL-U'
     MY_USER_ID_BOT = '5688833678'
+    
     MY_USER_ID = ''
     MSG_FINISH = 'El Bot, ya no esta escuchando...'
-    
-    #HAGGASASA
-    
