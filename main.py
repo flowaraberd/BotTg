3a7f403ae5c1264471453f5cd87e7b0b13ad6c9d
flowaraberd <67209635+flowaraberd@users.noreply.github.com>
2022-09-04 02:37:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -27,20 +27,3 @@ async def main():
 
 if __name__ == '__main__':
     asyncio.run(main())
-
-
-
-    
-# {'update_id': 451297745, 'message': {'group_chat_created': False, 'caption_entities': [], 
-# 'delete_chat_photo': False, 
-# 'chat': {'username': 'arabeltz', 'first_name': 'Moor', 'id': 1822798056, 'last_name': 'Mood', 'type': 'private'}, 
-# 'new_chat_photo': [], 
-# 'date': 1662247580, 
-# 'new_chat_members': [], 
-# 'entities': [], 
-# 'message_id': 2, 
-# 'channel_chat_created': False, 
-# 'text': 'hola', 
-# 'photo': [], 
-# 'supergroup_chat_created': False, 
-# 'from': {'id': 1822798056, 'first_name': 'Moor', 'username': 'arabeltz', 'language_code': 'es', 'is_bot': False, 'last_name': 'Mood'}}}
