82ebed1af216422dcf9386567d8643671e9692c5
flowaraberd <flowaraberd@gmail.com>
2022-09-04 03:16:27 -0300

    Bot con la configuracion por defecto

---
+++ Config.py
@@ -0,0 +1,9 @@
+
+from ast import expr
+
+
+class M:
+    MY_TOKEN_BOT = '5771454790:AAEsG6uwZM31InopK2nA3YDjzfJ7-TmDs08'
+    MY_USER_ID_BOT = '5771454790'
+    MY_USER_ID = ''
+    MSG_FINISH = 'El Bot, ya no esta escuchando...'
